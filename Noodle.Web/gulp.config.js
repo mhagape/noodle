72751794d13e72e22285a8440754376aa73d1112
unknown <grozaweb@gmail.com>
2015-09-29 22:19:06 +0300

    Add gulp task to compile typescript files

---
+++ Noodle.Web/gulp.config.js
@@ -0,0 +1,17 @@
+﻿
+module.exports = function () {
+  var config = {
+    ts: {
+      files: [
+       "app/**/*.ts",
+       "scripts/typings/**/*.ts"
+      ],
+      options: {
+        noImplicitAny: false,
+        module: "amd"
+      },
+      dest: "app"
+    }
+  };
+  return config;
+};
---
+++ Noodle.Web/gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require("gulp");
+var args = require("yargs").argv;
+var config = require("./gulp.config")();
+var $ = require("gulp-load-plugins")({ lazy: true });
+
+gulp.task("compile-typescript", function () {
+    console.log('Compiling typescript');
+    return gulp.src(config.ts.files)
+           .pipe($.typescript(config.ts.options))
+           .js
+           .pipe(gulp.dest(config.ts.dest));
+});
